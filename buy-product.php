5f755a963b916711c12efbfa1bf9aabcab88da88
panchai <thongtank@gmail.com>
2016-01-29 10:15:53 +0700

    add cash button

--- buy-product.php
+++ buy-product.php
@@ -135,7 +135,8 @@ $count_order = $order->count_order_by_product_id();
         </div>
         <div class="row content">
             <div class="col-md-12 ">
-                <button onclick="return confirm('ยืนยันข้อมูลรายการซื้อหรือไม่ ?');" type="submit" class="btn btn-raised btn-primary pull-right"><i class="fa fa-shopping-cart"></i> สั่งซื้อ</button>
+                <button onclick="return confirm('ยืนยันข้อมูลรายการซื้อด้วยเงินสด (Cash) หรือไม่ ?');" type="submit" class="btn btn-raised btn-success pull-right"><i class="fa fa-money"></i> สั่งซื้อด้วยเงินสด (Cash)</button>
+                <button onclick="return confirm('ยืนยันข้อมูลรายการซื้อด้วยเครดิต (Credit) หรือไม่ ?');" type="submit" class="btn btn-raised btn-warning pull-right"><i class="fa fa-credit-card"></i> สั่งซื้อด้วยเครดิต (Credit)</button>
                 <button type="button" class="btn btn-raised btn-primary pull-right" id="btt_plus"><i class="fa fa-plus"></i></button>
             </div>
         </div>
